e9cb956feca52238f3a79fdbffce024370cb4074
ErickM <erickmwarama@gmail.com>
2025-11-08 15:45:28 +0300

    fetch chirps from db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,10 @@
 <x-layout>
     <x-slot:title>
         Welcome
-</x-slot:title>
-    <div class="max-w-2xl mx-auto">
-        @foreach($chirps as $chirp)
-                <div class="card bg-base-100 shadow mt-8">
-                    <div class="card-body">
-                        <div>
-                            <div class="font-semibold">{{ $chirp['author'] }}</div>
-                            <div class="mt-1">{{ $chirp['message'] }}</div>
-                            <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @endforeach
+    </x-slot:title>
+        @forelse($chirps as $chirp)
+                <x-chirp :chirp="$chirp" />
+        @empty
+            <p>No chirps yet. Be the first to chirp! </p>
+        @endforelse
         </x-layout>
